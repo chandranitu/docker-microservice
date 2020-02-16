ec8d55473dfb4c624f56ff1778e55c28b8c02308
chandranitu <chandranitu@gmail.com>
2020-02-16 05:52:09 +0530

    added file

--- src/com/ness/Application.java
+++ src/com/ness/Application.java
@@ -2,9 +2,13 @@ package com.ness;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration (exclude = { DataSourceAutoConfiguration.class })
+//@SpringBootApplication (exclude = { DataSourceAutoConfiguration.class })
 
 //spring boot sample code
 
